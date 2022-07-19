bfb79a323da89a24906add6fd33b66e0349b5a41
Bianca Silva <biancaflorianodasilva@gmail.com>
2022-07-19 20:07:29 -0300

    Aula 02
    Gerando figurinhas para WhatsApp
    
    • Leitura de imagem (por arquivo e URL)
    • Criação de imagens com transparência e redimencionadas
    • Geração de nova imagem (em memória) a partir da original
    • Configuração do texto e escrita/edição da imagem
    • Gravar imagem em um novo arquivo .png

--- AllStickers/src/App.java
+++ AllStickers/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -11,29 +13,37 @@ public class App
     public static void main(String[] args) throws Exception 
     {
         // Conexão HTTP + Busca Top250
-        String url = "https://api.mocki.io/v2/549a5d8b";
-        URI endereco = URI.create(url);
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
+        URI address = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());       
         String body = response.body();        
 
         // Extração de Dados (título, poster, classificação)
         JsonParser parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        List<Map<String, String>> MoviesList = parser.parse(body);
         
         /* Testes
         System.out.println(body);
-        System.out.println(listaDeFilmes.size());
-        System.out.println(listaDeFilmes.get(0));
+        System.out.println(MoviesList.size());
+        System.out.println(MoviesList.get(0));
         */
 
-        // Manipulação de Dados
-        for (Map<String, String> filme : listaDeFilmes) 
+        // Manipulação de Dados   
+        var generator = new StickerGenerator();     
+        for (Map<String, String> movie : MoviesList) 
         {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            String urlImage = movie.get("image");
+            String title = movie.get("title");
+
+            InputStream inputStream = new URL(urlImage).openStream();
+            String archiveName = title + ".png";
+
+            generator.createSticker(inputStream, archiveName);
+
+            System.out.println(title);
+            System.out.println(movie.get("imDbRating"));
             System.out.println();
         }
     }
